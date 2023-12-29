401960f9d60b4926c128bccc63fe345dd049ccf6
23745444 <23745444@sun.ac.za>
2023-12-29 09:53:17 +0200

    from fix_home

--- application/frontend.js
+++ application/frontend.js
@@ -43,16 +43,31 @@ app.use(express.static('./application/public'))
 
 // Home
 app.get('/', (req, res, next) => {
-  axios.get('blogs')
-  .then(response => {
-    res.render('card', {count: response.data.count, blogs: response.data.blogs})
-  })
-  .catch(err => {
-    res.status(500).json({
-      message: err
-    })
-  })
+  axios
+    .get('blogs')
+    .then(response => {
+      const index = Math.floor(Math.random() * 5)
 
+      response.data.blogs[index].content = response.data.blogs[
+        index
+      ].content.replaceAll('\n', '<br>')
+
+      for (let i = 0; i < response.data.blogs.length; i++) {
+        response.data.blogs[i].author.userAvatar =
+        process.env.API_BASE_URL + '' + response.data.blogs[i].author.userAvatar
+      }
+
+      res.render('card', {
+        count: response.data.count,
+        blogs: response.data.blogs,
+        blogIndex: index
+      })
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err
+      })
+    })
 })
 
 app.post('/signup', (req, res, next) => {
