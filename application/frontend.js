5fc6b980b50de16f405ff1d512acca3e6b707820
23745444 <23745444@sun.ac.za>
2023-12-29 10:10:32 +0200

    routing works

--- application/frontend.js
+++ application/frontend.js
@@ -70,11 +70,9 @@ app.get('/', (req, res, next) => {
     })
 })
 
-app.post('/signup', (req, res, next) => {
+app.get('/signup', (req, res, next) => {
   console.log(req.body)
-  res.status(200).json({
-    message: 'ok'
-  })
+  res.render('user/sign')
 })
 
 module.exports = app
