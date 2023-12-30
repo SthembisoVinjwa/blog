1a0f578b83a42f9eea89067baf03521d4dfa9e87
KutlwanoMandla <Kutlwanomasombuka324@gmail.com>
2023-12-30 02:24:12 +0200

    sign up page fixed(Alignments)

--- application/public/user-script.js
+++ application/public/user-script.js
@@ -1,13 +1,19 @@
 function inOrUp () {
   if (document.querySelector('.sign').innerHTML == 'Sign In') {
     document.querySelector('.signup-form-container').style.cssText =
-      'display: block;'
+      `display: flex;
+      justify-content:center;
+      align-items:center;
+      flex-direction: column;`
     document.querySelector('.login-form-container').style.cssText =
       'display: none;'
     document.querySelector('.sign').innerHTML = 'Log In'
   } else {
     document.querySelector('.login-form-container').style.cssText =
-      'display: block;'
+    `display: flex;
+    justify-content:center;
+    align-items:center;
+    flex-direction: column;`
     document.querySelector('.signup-form-container').style.cssText =
       'display: none;'
     document.querySelector('.sign').innerHTML = 'Sign In'
